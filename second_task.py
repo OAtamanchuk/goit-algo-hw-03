bbf2b1e2333bbde37e85c55501bb1452410124c0
OAtamanchuk <aleksa.17.atama@gmail.com>
2025-08-11 13:53:30 +0300

    Second task fixed.

--- second_task.py
+++ second_task.py
@@ -2,10 +2,10 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     if (
-        not (1 <= min <= 1000)
-        or not (1 <= max <= 1000)
-        or min >= max
-        or not (min <= quantity <= max - min + 1)
+        not (1 <= min <= 1000) or
+        not (1 <= max <= 1000) or
+        min >= max or
+        not (1 <= quantity <= (max - min + 1))
     ):
         return []
 
@@ -14,7 +14,14 @@ def get_numbers_ticket(min, max, quantity):
         numbers.add(random.randint(min, max))
 
     return sorted(numbers)
-
-lottery_numbers = get_numbers_ticket(1, 117, 10)
-print("Ваші лотерейні числа:", lottery_numbers)
+   
+try:
+    min_value = int(input("Enter the minimum number: "))
+    max_value = int(input("Enter the maximum number: "))
+    quantity_value = int(input("Enter the quantity of numbers: "))
+    lottery_numbers = get_numbers_ticket(min_value, max_value, quantity_value)
+    print("Ваші лотерейні числа:", lottery_numbers)
+except ValueError:
+    print("Invalid input. Please enter valid integers for min, max, and quantity.")
+    
 
